65f19bb2c27557fdc45873919d9be3e588b47588
Taemin Kim <taeminkimny@gmail.com>
2022-05-17 13:49:27 -0400

    commit tree 45c4837d5f7eba71e91f05de9e222faa2cc3fc4f

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = drivers => drivers.slice(0,2)
+
+
+
+const returnLastTwoDrivers = drivers => drivers.slice(-2)
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier, num) {
+    return multiplier = num => num + num *4
+}
+
+const fareDoubler = num => num * 2 
+const fareTripler = num => num * 3 
+
+function selectDifferentDrivers(drivers, oneOfTwo) {
+    return oneOfTwo === returnFirstTwoDrivers ? returnFirstTwoDrivers(drivers) : returnLastTwoDrivers(drivers)
+}
